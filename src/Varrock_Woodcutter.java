8fb362286bd6595c47758ae39a1cc5e7ea2f7602
Bhavandeep Dhillon <bhavandeepdhillon@Bhavandeeps-MacBook-Pro.local>
2021-11-07 15:32:55 -0800

    Add all Varrock East Oak Cutting Files

---
+++ src/Varrock_Woodcutter.java
@@ -0,0 +1,84 @@
+
+import org.osbot.rs07.api.map.Position;
+import org.osbot.rs07.api.map.constants.Banks;
+import org.osbot.rs07.api.model.RS2Object;
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+import java.util.Arrays;
+
+@ScriptManifest(author = "You", info = "My first script", name = "Varrock Woodcutter", version = 0, logo = "")
+public class Varrock_Woodcutter extends Script
+{
+    //Walking Back Paths:
+    private Position[] path1 = {
+            new Position(3262, 3428, 0), new Position(3268, 3428, 0), new Position(3274, 3428, 0),
+            new Position(3279, 3423, 0), new Position(3281, 3416, 0)
+    };
+
+    //add second walking path and randomize between the two
+
+
+    //Banking Function:
+    void banking() throws InterruptedException
+    {
+        RS2Object bank1 = getObjects().closest("Bank booth");
+        bank1.interact("Bank");
+        sleep(912);
+        sleep(900);
+        getBank().deposit("Oak logs", 99);
+        sleep(934);
+        sleep(934);
+        getBank().close();
+    }
+
+    //Walking Back Function:
+    void walkBack() throws InterruptedException
+    {
+        getWalking().walkPath(Arrays.asList(path1));
+        sleep(900);
+        sleep(900);
+    }
+
+
+    //Executes once on start
+    @Override
+    public void onStart() {
+        log("Let's get started!");
+    }
+
+    @Override
+    public int onLoop() throws InterruptedException {
+        RS2Object tree = getObjects().closest("Oak");
+        if (inventory.isFull())
+        {
+            log("Inventory full, walking to bank");
+            walking.webWalk(Banks.VARROCK_EAST);
+            sleep(900);
+            sleep(900);
+            banking();
+            sleep(900);
+            sleep(400);
+            walkBack();
+        }
+        else if (tree != null && !myPlayer().isAnimating() && !myPlayer().isMoving())
+        {
+            log("Tree is present!");
+            tree.interact("Chop down");
+            sleep(1100);
+        }
+
+
+
+
+
+        return random(400, 900);
+    }
+
+    @Override
+    public void onExit() {
+        log("Thanks for running my Tea Thiever!");
+    }
+
+
+}
