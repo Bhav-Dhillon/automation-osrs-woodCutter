a64038afeac3a3b82bc65f97e864738e5095cce1
Dhillon <88413313+Bhav-Dhillon@users.noreply.github.com>
2022-09-08 00:00:18 -0700

    Update Varrock_Woodcutter.java

--- src/Varrock_Woodcutter.java
+++ src/Varrock_Woodcutter.java
@@ -7,19 +7,20 @@ import org.osbot.rs07.script.ScriptManifest;
 
 import java.util.Arrays;
 
-@ScriptManifest(author = "Dhillon", info = "WoodCutter", name = "Galactic Wood-Cutter", version = 0.0.1, logo = "")
+@ScriptManifest(author = "B.Dhillon", info = "WoodCutter", name = "Galactic Wood-Cutter", version = 0.0.1, logo = "")
+
 public class Varrock_Woodcutter extends Script
 {
     //Walking Back Paths:
     private Position[] path1 = {
-            new Position(3262, 3428, 0), new Position(3268, 3428, 0), new Position(3274, 3428, 0),
-            new Position(3279, 3423, 0), new Position(3281, 3416, 0)
+            new Position(3262, 3428, 0), 
+            new Position(3268, 3428, 0), 
+            new Position(3274, 3428, 0),
+            new Position(3279, 3423, 0), 
+            new Position(3281, 3416, 0)
     };
 
-    //add multiple walking paths and randomize between them.
-
-
-    //Banking Function:
+    // Banking Function:
     void banking() throws InterruptedException
     {
         RS2Object bank1 = getObjects().closest("Bank booth");
@@ -34,8 +35,7 @@ public class Varrock_Woodcutter extends Script
     void walkBack() throws InterruptedException
     {
         getWalking().walkPath(Arrays.asList(path1));
-        sleep(900);
-        sleep(900);
+        sleep(1800);
     }
 
 
@@ -48,28 +48,22 @@ public class Varrock_Woodcutter extends Script
     @Override
     public int onLoop() throws InterruptedException {
         RS2Object tree = getObjects().closest("Oak");
-        if (inventory.isFull())
-        {
+        
+        if (inventory.isFull()) {
             log("Inventory full, walking to bank");
             walking.webWalk(Banks.VARROCK_EAST);
-            sleep(900);
-            sleep(900);
+            sleep(1800);
             banking();
-            sleep(900);
-            sleep(400);
+            sleep(1300);
             walkBack();
         }
-        else if (tree != null && !myPlayer().isAnimating() && !myPlayer().isMoving())
-        {
+        
+        else if (tree != null && !myPlayer().isAnimating() && !myPlayer().isMoving() {
             log("Tree is present!");
             tree.interact("Chop down");
             sleep(1100);
         }
-
-
-
-
-
+        
         return random(400, 900);
     }
 
